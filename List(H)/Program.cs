6d8893f417d739e6e71c4aec796c368902d07385
raysc <raysc@RYA>
2024-03-16 19:18:27 -0400

    Add project files.

---
+++ List(H)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+  
+        List<string> playersList = new List<string> { "Chris", "James", "Mark", "Ethan", "Brad" };
+
+       
+        playersList.AddRange(new string[] { "Alex", "Luke", "Sam" });
+
+      
+        playersList.Sort();
+        Console.WriteLine("Sorted List:");
+        foreach (string player in playersList)
+        {
+            Console.WriteLine(player);
+        }
+
+      
+        playersList.Remove("Luke");
+
+       
+        playersList.Reverse();
+        Console.WriteLine("\nList in Reverse Order:");
+        foreach (string player in playersList)
+        {
+            Console.WriteLine(player);
+        }
+    }
+}
